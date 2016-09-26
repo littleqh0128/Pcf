a166455bfb40afe33925c35e77b9543218589e89
qianhao <littleqh@sina.com>
2016-09-26 16:55:29 +0800

    add project

---
+++ src/main/java/com/wondersgroup/pcf/baseframe/base/BaseBo.java
@@ -0,0 +1,118 @@
+package com.wondersgroup.pcf.baseframe.base;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Transient;
+
+import org.joda.time.DateTime;
+
+@MappedSuperclass
+public abstract class BaseBo implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	/* 创建人 */
+	private String stCreator;
+	/* 创建时间 */
+	private DateTime dtCreate;
+	/* 创建时间 :时间段查询用到，@Transient*/
+	private DateTime dtCreateBegin;
+	/* 创建时间:时间段查询时用到 @Transient*/
+	private DateTime dtCreateEnd;
+	/* 最后修改人 */
+	private String stModifier;
+	/* 最后修改时间 */
+	private DateTime dtModify;
+	/* 删除标志 */
+	private int removed;
+	/* 删除人 */
+	private String stRemover;
+	/* 删除时间 */
+	private DateTime dtRemove;
+	
+	public BaseBo() {
+		this.removed = 0;
+	}
+
+	@Column(name = "ST_CREATOR")
+	public String getStCreator() {
+		return stCreator;
+	}
+
+	public void setStCreator(String stCreator) {
+		this.stCreator = stCreator;
+	}
+	
+	@Column(name = "DT_CREATE")
+	public DateTime getDtCreate() {
+		return dtCreate;
+	}
+
+	public void setDtCreate(DateTime dtCreate) {
+		this.dtCreate = dtCreate;
+	}
+
+	@Column(name = "ST_MODIFIER")
+	public String getStModifier() {
+		return stModifier;
+	}
+
+	public void setStModifier(String stModifier) {
+		this.stModifier = stModifier;
+	}
+	
+	@Column(name = "DT_MODIFY")
+	public DateTime getDtModify() {
+		return dtModify;
+	}
+
+	public void setDtModify(DateTime dtModify) {
+		this.dtModify = dtModify;
+	}
+	
+	@Column(name = "REMOVED")
+	public int getRemoved() {
+		return removed;
+	}
+	
+	public void setRemoved(int removed) {
+		this.removed = removed;
+	}
+	
+	@Column(name = "ST_REMOVER")
+	public String getStRemover() {
+		return stRemover;
+	}
+
+	public void setStRemover(String stRemover) {
+		this.stRemover = stRemover;
+	}
+	
+	@Column(name = "DT_REMOVE")
+	public DateTime getDtRemove() {
+		return dtRemove;
+	}
+
+	public void setDtRemove(DateTime dtRemove) {
+		this.dtRemove = dtRemove;
+	}
+	
+	@Transient
+	public DateTime getDtCreateBegin() {
+		return dtCreateBegin;
+	}
+
+	public void setDtCreateBegin(DateTime dtCreateBegin) {
+		this.dtCreateBegin = dtCreateBegin;
+	}
+	
+	@Transient
+	public DateTime getDtCreateEnd() {
+		return dtCreateEnd;
+	}
+
+	public void setDtCreateEnd(DateTime dtCreateEnd) {
+		this.dtCreateEnd = dtCreateEnd;
+	}
+}
---
+++ src/main/java/com/wondersgroup/pcf/sys/model/bo/SysUser.java
@@ -0,0 +1,54 @@
+package com.wondersgroup.pcf.sys.model.bo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import com.wondersgroup.pcf.baseframe.base.BaseBo;
+
+@Entity
+@Table(name="SYS_USER")
+public class SysUser extends BaseBo {
+	
+	private static final long serialVersionUID = 1L;
+	
+	// 用户ID
+	private String stId;
+	
+	// 登录名
+	private String stLoginName;
+	
+	// 姓名
+	private String stName;
+
+	@Id  
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy="uuid")
+    @Column(name="ST_ID")
+	public String getStId() {
+		return stId;
+	}
+	public void setStId(String stId) {
+		this.stId = stId;
+	}
+	
+	@Column(name="ST_LOGIN_NAME")
+	public String getStLoginName() {
+		return stLoginName;
+	}
+	public void setStLoginName(String stLoginName) {
+		this.stLoginName = stLoginName;
+	}
+	
+	@Column(name="ST_NAME")
+	public String getStName() {
+		return stName;
+	}
+	public void setStName(String stName) {
+		this.stName = stName;
+	}
+}
